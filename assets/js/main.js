c68b4197a73db5e9edc5ca9ecff64140311ffc2a
Cypher <adityasaha444@gmail.com>
2020-10-02 22:06:44 +0530

    toggler moved to main.js

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// Navbar toggler
+function menuToggle() {
+  var nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  var toggle = document.getElementById('toggle');
+  toggle.classList.toggle('active');
+}
